1983c03eabb0283833c7a1c0e095f78d3190dc64
GitHub Action <action@github.com>
2022-07-23 00:44:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/hhinkel/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hhinkel/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hhinkel.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hhinkel.intellijplugin.MyBundle
 
 class MyApplicationService {
 
